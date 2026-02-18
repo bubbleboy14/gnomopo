5aaab554e4fd65b603553cc77a0de6596f6b4b74
bubbleboy14 <mario.balibrera@gmail.com>
2026-02-17 22:05:14 -0800

    onconnection() - []-wrap ibytes

--- gnomopo/extension.js
+++ gnomopo/extension.js
@@ -30,7 +30,7 @@ export default class GnomopoExtension extends Extension {
     onconnection(socket_service, connection, channel) {
         const istream = connection.get_input_stream(),
             ibytes = istream.read_bytes(4, null).get_data(),
-            action = String.fromCharCode.apply(null, ibytes);
+            action = String.fromCharCode.apply(null, [ibytes]);
         log("processing " + action);
         let resp, x, y, primon, geo;
         if (action == "mpos")
