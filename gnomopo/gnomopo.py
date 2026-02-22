71bdb8544117687d346991e9c1c594d6da3c8d4f
bubbleboy14 <mario.balibrera@gmail.com>
2026-02-21 17:26:40 -0800

    getwindow() and getsize()/getpos() tweax

--- gnomopo/gnomopo.py
+++ gnomopo/gnomopo.py
@@ -4,14 +4,20 @@ from fyg.util import confirm, cmd
 from .util import setverbosity, log, getres
 from .installer import Installer
 
-def getpos(addr="127.0.0.1", port=62090):
-	return getres("mpos", addr, port)
+def getpos(withmods=False, addr="127.0.0.1", port=62090):
+	res = getres("mpos", addr, port)
+	return res if withmods else res[:2]
 
-def getsize(addr="127.0.0.1", port=62090):
-	return getres("size", addr, port)
+def getsize(withscale=True, addr="127.0.0.1", port=62090):
+	res = getres("size", addr, port)
+	return res if withscale else res[:2]
+
+def getwindow(rect="both", addr="127.0.0.1", port=62090):
+	res = getres("window", addr, port)
+	return res if rect == "both" else res[rect] # frame or buffer
 
 def invoke():
-	parser = OptionParser("gnomopo [getpos|getsize|enable|disable|install|reinstall|uninstall] -v")
+	parser = OptionParser("gnomopo [getpos|getsize|getwindow|enable|disable|install|reinstall|uninstall] -v")
 	parser.add_option("-v", "--verbose", action="store_true", dest="verbose",
 		default=False, help="a few extra getpos/getsize logs")
 	ops, args = parser.parse_args()
@@ -20,11 +26,12 @@ def invoke():
 	if action.endswith("install") or action.endswith("able"):
 		os.chdir(os.path.abspath(__file__).rsplit("/", 1).pop(0))
 		Installer().run(action)
-	else:
-		if action == "getpos":
-			print(*getpos())
-		elif action == "getsize":
-			print(*getsize())
+	elif action == "getpos":
+		print(*getpos())
+	elif action == "getsize":
+		print(*getsize())
+	elif action == "getwindow":
+		print(getwindow())
 
 if __name__ == "__main__":
 	invoke()
