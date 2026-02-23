9fdc7672d860bd336298df8cafcf5e58652a3eda
bubbleboy14 <mario.balibrera@gmail.com>
2026-02-23 11:57:07 -0800

    refactored extension - new Gnomopo class

--- gnomopo/extension.js
+++ gnomopo/extension.js
@@ -32,50 +32,59 @@ const rectify = function(rect) {
     };
 };
 
+class Gnomopo {
+    mpos() {
+        return global.get_pointer().join(" ");
+    }
+
+    size() {
+        const primon = global.display.get_primary_monitor(),
+            geo = global.display.get_monitor_geometry(primon),
+            coords = [geo.width, geo.height, primon.geometry_scale || 1];
+        return coords.join(" ");
+    }
+
+    window() {
+        const fwin = global.display.get_focus_window();
+        return JSON.stringify({
+            "frame": rectify(fwin.get_frame_rect()),
+            "buffer": rectify(fwin.get_buffer_rect())
+        });
+    }
+
+    process(action) {
+        if (! ["mpos", "size", "window"].includes(action))
+            return "illegal action";
+        return this[action]();
+    }
+}
+
 export default class GnomopoExtension extends Extension {
     onconnection(socket_service, connection, channel) {
         const datastream = new Gio.DataInputStream({
             base_stream: connection.get_input_stream()
-        });
+        }), proc = this._gnomopo.process.bind(this._gnomopo);
+        log("opened");
         datastream.read_line_async(GLib.PRIORITY_DEFAULT, null, (stream, res) => {
             try {
                 const [line, length] = stream.read_line_finish(res);
-                if (line === null) {
+                if (length == 0 || line === null) {
                     connection.close(null);
-                    return;
+                    return log("closed");
                 }
-                const action = line.toString().trim();
-                let coords, resp, primon, geo, frect, brect;
-                if (action == "mpos")
-                    coords = global.get_pointer();
-                else if (action == "size") {
-                    primon = global.display.get_primary_monitor();
-                    geo = global.display.get_monitor_geometry(primon);
-                    coords = [geo.width, geo.height, primon.geometry_scale || 1];
-                } else if (action == "window") {
-                    frect = global.display.get_focus_window().get_frame_rect();
-                    brect = global.display.get_focus_window().get_buffer_rect();
-                    resp = JSON.stringify({
-                        "frame": rectify(frect),
-                        "buffer": rectify(brect)
-                    });
-                }
-                else
-                    resp = "illegal action";
-                if (!resp)
-                    resp = coords.join(" ");
+                const action = line.toString().trim(), resp = proc(action);
                 log(action + " " + resp);
                 connection.get_output_stream().write_bytes(new GLib.Bytes(resp), null);
             } catch (e) {
-                log("error: " + e.message);
-            } finally {
                 connection.close(null);
+                log("closed with error: " + e.message);
             }
         });
         return true;
     }
 
     enable() {
+        this._gnomopo = new Gnomopo();
         this._service = new Gio.SocketService();
         this._service.add_address(socket_address, Gio.SocketType.STREAM, Gio.SocketProtocol.DEFAULT, null);
         this._service.connect('incoming', this.onconnection.bind(this));
